2a2781686d331c6af50b6e4f0320c464d531a275
Ismael Martinez Ramos <ismaelmartinez@gmail.com>
2026-03-03 09:54:10 +0000

    fix: handle nested parentheses in dangerous markdown links

--- internal/comment/sanitize.go
+++ internal/comment/sanitize.go
@@ -3,7 +3,7 @@ package comment
 import "regexp"
 
 var (
-	dangerousLinkRe = regexp.MustCompile(`\[([^\]]*)\]\((javascript|data|vbscript):[^)]*\)`)
+	dangerousLinkRe = regexp.MustCompile(`\[(.*?)\]\((?i:javascript|data|vbscript):.*\)`)
 	scriptTagRe     = regexp.MustCompile(`(?is)<script[^>]*>.*?</script>`)
 	htmlTagRe       = regexp.MustCompile(`<[^>]*>`)
 )
